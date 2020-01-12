7151cada68871536db190865593ebed216caef90
Emmanuel Croccel <emmanuel.croccel@outlook.fr>
2020-01-12 05:47:49 +0100

    Addind Serialization of Instruction - Better rendering of API response

--- src/main/java/com/sopra/artemis/modeles/Instruction.java
+++ src/main/java/com/sopra/artemis/modeles/Instruction.java
@@ -1,28 +1,57 @@
 package com.sopra.artemis.modeles;
 
+import java.io.Serializable;
 import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 
-public class Instruction {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-		@Id
-		public String id;
-		
-		public String typeInstruction;
-		
-		public String ordreLancement;
-		
-		public Map<String,String> parametresInstruction;
-		
-		public Instruction(String typeInstruction,String ordreLancement,Map<String,String> parametresInstruction) {
-			this.typeInstruction=typeInstruction;
-			this.ordreLancement=ordreLancement;
-			this.parametresInstruction=parametresInstruction;
-		}
-		
-		@Override
-		public String toString() {
-			return String.format("Instruction[id='%s', typeInstruction='%s', ordreLancement='%s', parametresInstruction='%s'",id,typeInstruction,ordreLancement,parametresInstruction);
-		}
+public class Instruction implements Serializable {
+
+	public String typeInstruction;
+
+	public String ordreLancement;
+
+	public Map<String, String> parametresInstruction;
+
+	public String getTypeInstruction() {
+		return typeInstruction;
+	}
+
+	public void setTypeInstruction(String typeInstruction) {
+		this.typeInstruction = typeInstruction;
+	}
+
+	public String getOrdreLancement() {
+		return ordreLancement;
+	}
+
+	public void setOrdreLancement(String ordreLancement) {
+		this.ordreLancement = ordreLancement;
+	}
+
+	public Map<String, String> getParametresInstruction() {
+		return parametresInstruction;
+	}
+
+	public void setParametresInstruction(Map<String, String> parametresInstruction) {
+		this.parametresInstruction = parametresInstruction;
+	}
+
+	public Instruction() {
+	}
+
+	public Instruction(String typeInstruction, String ordreLancement, Map<String, String> parametresInstruction) {
+		this.typeInstruction = typeInstruction;
+		this.ordreLancement = ordreLancement;
+		this.parametresInstruction = parametresInstruction;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Instruction[typeInstruction='%s', ordreLancement='%s', parametresInstruction='%s'",
+				typeInstruction, ordreLancement, parametresInstruction);
+	}
 }
--- src/main/java/com/sopra/artemis/modeles/QualityModele.java
+++ src/main/java/com/sopra/artemis/modeles/QualityModele.java
@@ -1,31 +1,59 @@
 package com.sopra.artemis.modeles;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 
-public class QualityModele {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+public class QualityModele implements Serializable {
 
 	@Id
 	public String id;
-	
+
 	public String nom;
-	
-	public Map<String,String> qualityHeader;
-	
+
+	public Map<String, String> qualityHeader;
+
 	List<Instruction> instructions;
-	
-	
-	public QualityModele(String nom, Map<String,String> qualityHeader,List<Instruction> instructions) {
-		this.nom=nom;
-		this.qualityHeader=qualityHeader;
-		this.instructions=instructions;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Map<String, String> getQualityHeader() {
+		return qualityHeader;
+	}
+
+	public void setQualityHeader(Map<String, String> qualityHeader) {
+		this.qualityHeader = qualityHeader;
+	}
+
+	public List<Instruction> getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(List<Instruction> instructions) {
+		this.instructions = instructions;
+	}
+
+	public QualityModele(String nom, Map<String, String> qualityHeader, List<Instruction> instructions) {
+		this.nom = nom;
+		this.qualityHeader = qualityHeader;
+		this.instructions = instructions;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("QualityModele[id='%s',nom='%s', qualityHeader='%s', instructions='%s'", id,nom, qualityHeader,instructions);
+		return String.format("QualityModele[id='%s',nom='%s', qualityHeader='%s', instructions='%s'", id, nom,
+				qualityHeader, instructions);
 	}
-	
+
 }
